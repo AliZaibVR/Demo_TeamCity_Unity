72565c7acb6a48bda33a1f684747a4dc08575afb
AliZaib35 <ali.zaib.3553@gmail.com>
2017-12-29 13:41:25 +0500

    ArthurBuildSettingupdated

--- Unity/BUILD AUTOMATION/Assets/Scripts/ArthurBuildSettings.cs
+++ Unity/BUILD AUTOMATION/Assets/Scripts/ArthurBuildSettings.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameBuildSettings", menuName = "Game Build Settings", order = 1)]
 public class GameBuildSettings : ScriptableObject
 {
-    public GameBuildType gameRegion;
+    public GameBuildType GameType;
+    public bool isVRSupported;
+    public bool isDebugBuild;
+    public string GameRegion;
+    public string ConnectionIP;
+    public string ServerAccessKey;
 
 }
 public enum GameBuildType
